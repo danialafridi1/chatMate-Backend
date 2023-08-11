ddcbb2399c2c380c3139bcce398eba50f40cd65b
danial <redkhan13@gmail.com>
2023-08-12 04:41:49 +0500

    add required lib in project

--- index.js
+++ index.js
@@ -1,8 +1,45 @@
 const express = require('express')
-const app = express()
-const routes = require('./routes');
 require('dotenv').config();
+const routes = require('./routes');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const mongoSenitize = require('express-mongo-sanitize');
+const cookieParser = require('cookie-parser');
+const compresion = require('compression');
+const fileupload = require('express-fileupload');
+const cors = require('cors');
+const app = express()
 
+
+// using morgan middleware
+if (process.env.NODE_ENV !== "production") {
+    app.use(morgan('combined'));
+}
+
+// adding helmet 
+app.use(helmet());
+// parser json
+app.use(express.json());
+// parser url
+app.use(express.urlencoded({extended : true}));
+// use mongo sanitize 
+app.use(mongoSenitize());
+// enable cookie parser
+app.use(cookieParser())
+// enable data body compression
+app.use(compresion());
+// fileupload
+app.use(fileupload({
+    useTempFiles : true
+}));
+// useing cors
+app.use(cors(
+    {
+        origin : "http://localhost:3000"
+    }
+))
+// implement routes 
 app.use(routes);
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(process.env.PORT, () => console.log(`Example app listening on port ${process.env.PORT}!`))
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
